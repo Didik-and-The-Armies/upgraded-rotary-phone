610791b1f8d68f0d68916737a31950f8d0cb9ac7
Abda Shaffan Diva <abdashaffan@gmail.com>
2018-11-19 14:35:03 +0700

    Edit items

--- items.pl
+++ items.pl
@@ -1,2 +1,30 @@
 /*File ini berisi jenis-jenis item yang ada pada pada game PUBG*/
 
+/* Fakta Armor */
+
+/*armortype(nama_armor, health_armor)*/
+armortype(armor1,20).
+armortype(armor2,30).
+armortype(armor3,40).
+
+/* Fakta Senjata */
+
+/*weapontype(nama_senjata, damage_senjata, jumlah_peluru_senjata(?))*/
+weapontype(rifle,30,5).
+weapontype('submachine gun',20,4).
+weapontype(sniper,50,2).
+
+
+/* Fakta Medicine */
+/* medicinetype(nama_medicine,health_capacity_medicine)*/
+medicinetype(perban, 10).
+medicinetype(anestisi, 20).
+medicinetype(ganja,50).
+
+/* Fakta Ammo */
+/* ammotype(nama_senjata,isi_pak_peluru)*/
+ammotype(rifle,3).
+ammotype('submachine gun',2).
+ammotype(sniper,1).
+
+
